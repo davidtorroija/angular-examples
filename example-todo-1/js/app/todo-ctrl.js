2a4666c55eaa02ce910aa50a00b7e853af6fc5d9
daneryl <daneryl@txau-2.local>
2013-09-26 10:53:42 -0500

    fixed some tests

--- example-todo-1/js/app/todo-ctrl.js
+++ example-todo-1/js/app/todo-ctrl.js
@@ -13,22 +13,28 @@ angular.module('todoApp')
 		//aca le pregunto si estas definido y tenes un 
 		//valor valido esto es diferente de empty ahi recien 
 		//entra si no no entres	
-		console.log('push',$scope.newTodo);		
 			$scope.todos.push({description:$scope.newTodo});
 		}
 	}	
-	function isValidTodo() {
-		if(!$scope.newTodo){			 
-			return false;						
-		}
-		//con underscore es mas facil pero agregariamos complejidad
-		angular.forEach($scope.todos, function(todo, key){
-			console.log(todo,$scope.newTodo,todo.description===$scope.newTodo);
-			if(todo.description===$scope.newTodo){
-				console.log('entro aca')
-				return false;
-			}
-		});
-		return true;
-	}
+  String.prototype.strcmp = function(s) {
+    if (this < s) return -1;
+    if (this > s) return 1;
+    return 0;
+  }
+
+  function isValidTodo() {
+    if(!$scope.newTodo){			 
+      return false;						
+    }
+
+    var is_valid = true;
+
+    //con underscore es mas facil pero agregariamos complejidad
+    angular.forEach($scope.todos, function(todo, key){
+      if(todo.description === $scope.newTodo){
+        is_valid = false;
+      }
+    });
+    return is_valid;
+  }
 });
